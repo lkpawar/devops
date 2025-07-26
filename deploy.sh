29e98709894144c5ee4c7ecf569dfd48eb1212e9
lkpawar <lkpawar9090@gmail.com>
2025-07-26 20:30:35 +0530

    Adding few more things in deploy file

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,7 @@ echo "Deploying files from GitHub repo to /tmp/myapp"
 mkdir -p /tmp/myapp
 cp -v * /tmp/myapp/
 echo "✅ Deployment finished!"
+echo "🚀 Deploy script started"
+date
+hostname
+echo "✅ Deployment completed"
